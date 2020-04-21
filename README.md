8cf1bf73aade7c1a76f5a58b0112b4768c95b48e
thewonelolf <thewonelolf@github.com>
2020-04-21 14:54:59 -0400

    Added tool to check for missing sids in a DB

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # FFScraperTools
-A collection of tchotchkes and gizmos for dealing with the databases put out by the Fanfictioin Scraper V2
+A collection of tchotchkes and gizmos for dealing with the databases put out by the Fanfictioin Scraper V2.
 
 This is mostly stuff made for myself but may be useful for others trying to scrape fanfiction for whatever reason.
+
+These may be useful for dealing specifically with Fanfiction scrape databases, assuming all DBs are following the formatting used in this tool, and my scraper.
---
+++ dbtotality.py
@@ -0,0 +1,30 @@
+#Finds the lowest and highest SID in a .db file and searches for any missing SIDs in that range.
+
+import sqlite3
+
+pathDb = str(input("Filename for DB A:"))
+
+checkDB = sqlite3.connect(pathDb)
+
+dbCursor = checkDB.cursor()
+dbCursor.execute('SELECT * FROM fanfiction')
+dbOutput = dbCursor.fetchall()
+
+dbSIDS = []
+missingSIDS = []
+
+for entry in dbOutput:
+    dbSIDS.append(entry[0])
+
+dbMin = min(dbSIDS)
+dbMax = max(dbSIDS)
+
+
+for x in range(dbMin, dbMax):
+    print(x)
+    checkExist = dbCursor.execute('''SELECT id FROM fanfiction WHERE id=?''',(x,))
+    result = checkExist.fetchone()
+    if not result:
+        missingSIDS.append(x)
+
+print(missingSIDS)
