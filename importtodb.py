c8e1c1cd019fa73bc5c5280b7b16e8c7e35271bc
thewonelolf <thewonelolf@github>
2020-05-19 14:55:35 -0400

    Text File Imported fully functional

--- importtodb.py
+++ importtodb.py
@@ -1,5 +1,6 @@
 #Import to database from text files downloaded from internetarchive
 from urllib.parse import urlparse
+import sqlite3, os, tqdm
 
 def textfileparse(fileName):
     
@@ -15,7 +16,6 @@ def textfileparse(fileName):
     contents = contents.splitlines()
 
     paramDict["title"] = contents[3]
-    paramDict["authorid"] = contents[5][3:]
     
     x = 0
     for line in contents:
@@ -46,6 +46,58 @@ def textfileparse(fileName):
             storyString += str(line) + "\n "
         x+=1
     paramDict["story"] = storyString
-    print(paramDict)
+    return(paramDict)
 
-textfileparse(input("fileName: "))
+
+connection = sqlite3.connect('importFanfiction.db')
+c = connection.cursor()
+
+c.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='fanfiction' ''')
+
+if c.fetchone()[0]==0:
+    sql_command = ('''
+    CREATE TABLE fanfiction (
+        id INTEGER PRIMARY KEY,
+        canon_type TEXT,
+        canon TEXT,
+        authorid INTEGER,
+        title TEXT,
+        updated INTEGER,
+        published INTEGER,
+        language TEXT,
+        genre TEXT,
+        rating TEXT,
+        chapters INTEGER,
+        words INTEGER,
+        reviews INTEGER,
+        favs INTEGER,
+        follows INTEGER,
+        status INTEGER,
+        story TEXT
+    );''')
+    c.execute(sql_command)
+    print("Created Table")
+else:
+    print("Table exists")
+
+
+rootdir = input("Root Directory:")
+
+for fileName in os.walk(rootdir):
+    try:
+        if fileName[-1][0].endswith(".txt"):
+            fullPath = fileName[0] + "/" + fileName[2][0]
+            print("Importing", fileName[2][0])
+            storyDict = textfileparse(fullPath)
+            storyID = storyDict.get("id")
+            c.execute('SELECT 1 FROM fanfiction WHERE id=? LIMIT 1', (storyID,))
+            exists = c.fetchone()    
+            if exists == None:       
+                c.execute('''INSERT INTO fanfiction (id, canon_type, canon, authorid, title, updated, published, language, genre, rating, chapters, words, reviews, favs, follows, status, story)
+                                VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)''', (storyDict.get('id'), storyDict.get('canon_type'), storyDict.get('canon'), storyDict.get('authorid'), storyDict.get('title'), storyDict.get('updated'), storyDict.get('published'), storyDict.get('language'), storyDict.get('genre'), storyDict.get('rating'), storyDict.get('chapters'), storyDict.get('words'), storyDict.get('reviews'), storyDict.get('favs'), storyDict.get('follows'), storyDict.get('status'), storyDict.get("story")))
+                connection.commit()
+            else:
+                print("Story already in DB. ID:", storyID)
+    except IndexError:
+        pass
+    
