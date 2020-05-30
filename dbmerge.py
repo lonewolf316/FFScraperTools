f0f476bc35d9d9e530235c7a33b3da6f0fd2cef1
thewonelolf <thewonelolf@github>
2020-05-29 20:20:03 -0400

    Fixed merging for better performance and recovery after stopping

--- dbmerge.py
+++ dbmerge.py
@@ -1,6 +1,7 @@
 #Merge 2 fanfiction database files together. Creates a totally new file as to leave the old ones untouched.
 
 import sqlite3
+from tqdm import tqdm
 
 pathA = str(input("Filename for DB A:"))
 pathB = str(input("Filename for DB B:"))
@@ -9,57 +10,61 @@ pathC = str(input("Output file name:"))
 dbA = sqlite3.connect(pathA)
 dbB = sqlite3.connect(pathB)
 dbC = sqlite3.connect(pathC)
+cCursor = dbC.cursor()
 
-dbC.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='fanfiction' ''')
+cCursor.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='fanfiction' ''')
 
+if cCursor.fetchone()[0]==0:
+    sql_command = ('''
+    CREATE TABLE fanfiction (
+        id INTEGER PRIMARY KEY,
+        canon_type TEXT,
+        canon TEXT,
+        authorid INTEGER,
+        title TEXT,
+        updated INTEGER,
+        published INTEGER,
+        language TEXT,
+        genre TEXT,
+        rating TEXT,
+        chapters INTEGER,
+        words INTEGER,
+        reviews INTEGER,
+        favs INTEGER,
+        follows INTEGER,
+        status INTEGER,
+        story TEXT
+    );''')
 
-setupCommand = ('''
-CREATE TABLE fanfiction (
-    id INTEGER PRIMARY KEY,
-    canon_type TEXT,
-    canon TEXT,
-    authorid INTEGER,
-    title TEXT,
-    updated INTEGER,
-    published INTEGER,
-    language TEXT,
-    genre TEXT,
-    rating TEXT,
-    chapters INTEGER,
-    words INTEGER,
-    reviews INTEGER,
-    favs INTEGER,
-    follows INTEGER,
-    status INTEGER,
-    story TEXT
-);''')
-dbC.execute(setupCommand)
-
-aCursor = dbA.cursor()
-aCursor.execute('SELECT * FROM fanfiction')
-aOutput = aCursor.fetchall()
-
-for entry in aOutput:
-    print("Populating SID:"+str(entry[0]))
-    dbC.execute('''INSERT INTO fanfiction (id, canon_type, canon, authorid, title, updated, published, language, genre, rating, chapters, words, reviews, favs, follows, status, story)
-                            VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)''', entry)                       
-dbC.commit()
-
-del(aOutput)
-
-bCursor = dbB.cursor()
-bCursor.execute('SELECT * FROM fanfiction')
-bOutput = bCursor.fetchall()
+    cCursor.execute(sql_command)
+else:
+    print('Table exists')
 
 cCursor = dbC.cursor()
-for entry in bOutput:
+aCursor = dbA.cursor()
+aCursor.execute('SELECT * FROM fanfiction')
+
+for entry in aCursor:
     checkExist = cCursor.execute('''SELECT id FROM fanfiction WHERE id=?''',(int(entry[0]),))
     result = checkExist.fetchone()
     if result:
-        print("Already exists, skip SID: "+str(entry[0]))
-
+        print("Skipping:", entry[0])
     else:
-        print("Populating SID:"+str(entry[0]))
+        dbC.execute('''INSERT INTO fanfiction (id, canon_type, canon, authorid, title, updated, published, language, genre, rating, chapters, words, reviews, favs, follows, status, story)
+                                VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)''', entry)
+        print("Imported: ", entry[0])
+        dbC.commit()
+bCursor = dbB.cursor()
+bCursor.execute('SELECT * FROM fanfiction')
+
+
+for entry in bCursor:
+    checkExist = cCursor.execute('''SELECT id FROM fanfiction WHERE id=?''',(int(entry[0]),))
+    result = checkExist.fetchone()
+    if result:
+        print("Skipping:", entry[0])
+    else:
         dbC.execute('''INSERT INTO fanfiction (id, canon_type, canon, authorid, title, updated, published, language, genre, rating, chapters, words, reviews, favs, follows, status, story)
                                 VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)''', entry)   
-dbC.commit()
+        print("Imported: ", entry["id"])
+        dbC.commit()
